88fbe2a85ddb491dde90fbcc744cdf16612dcefc
Loren Aparicio <loren.djkiller@gmail.com>
2017-09-23 23:01:10 +0200

    Add files via upload

---
+++ UUDD_PIRAMIDE LETRAS/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim cadena As String = "ZYXWVTSRQPONMLKJIHGFEDCBA"
+        Dim contador As Integer = 0
+        Dim contador2 As Integer = 0
+        Dim tamaño As Integer
+
+        tamaño = cadena.Length
+
+        For contador = 1 To tamaño Step 1
+
+            Console.WriteLine(cadena.Remove(0, contador2))
+            contador2 = contador2 + 1
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
